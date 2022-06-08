ead991874d6d27790eae968a9bc3b984c46772d0
Jesse Mitchell <jesse@jmitchellmusic.com>
2022-06-07 19:17:11 -0700

    initial commit

---
+++ Item.java
@@ -0,0 +1,6 @@
+public class Item {
+    
+    // create Item class member variables
+    public String name;
+    public double price;
+}
---
+++ Order.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Order {
+
+    // create Order class member variables
+    public String name;
+    public double total;
+    public boolean ready;
+
+    public ArrayList<Item> items = new ArrayList<Item>();
+}
